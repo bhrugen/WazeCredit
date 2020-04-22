7a7b67bc9e41316d3eca9bdcab11b290ea1cac7b
Ben Spark <bhrugen1990@gmail.com>
2020-04-22 14:29:28 -0500

    Setting up Non DI Scenario Part 2

--- WazeCredit/Controllers/HomeController.cs
+++ WazeCredit/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WazeCredit.Models;
+using WazeCredit.Models.ViewModels;
+using WazeCredit.Service;
 
 namespace WazeCredit.Controllers
 {
@@ -13,8 +15,31 @@ namespace WazeCredit.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            HomeVM homeVM = new HomeVM();
+            MarketForecaster marketForecaster = new MarketForecaster();
+            MarketResult currentMarket = marketForecaster.GetMarketPrediction();
+
+            switch (currentMarket.MarketCondition)
+            {
+                case MarketCondition.StableDown:
+                    homeVM.MarketForecast= "Market shows signs to go down in a stable state! It is a not a good sign to apply for credit applications! But extra credit is always piece of mind if you have handy when you need it.";
+                    break;
+                case MarketCondition.StableUp:
+                    homeVM.MarketForecast = "Market shows signs to go up in a stable state! It is a great sign to apply for credit applications!";
+                    break;
+                case MarketCondition.Volatile:
+                    homeVM.MarketForecast = "Market shows signs of volatility. In uncertain times, it is good to have credit handy if you need extra funds!";
+                    break;
+                default:
+                    homeVM.MarketForecast = "Apply for a credit card using our application!";
+                    break;
+            }
+            return View(homeVM);
         }
+
+
+
+
 
         public IActionResult Privacy()
         {
