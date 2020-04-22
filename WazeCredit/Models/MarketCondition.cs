25417aa577fa6bdd43a0d9623ae8748f69a066b1
Ben Spark <bhrugen1990@gmail.com>
2020-04-22 14:19:10 -0500

    Setting up Non DI Scenario Part 1

---
+++ WazeCredit/Models/MarketCondition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WazeCredit.Models
+{
+    public enum MarketCondition
+    {
+        StableUp,
+        StableDown,
+        Volatile
+    }
+}
---
+++ WazeCredit/Service/MarketForecaster.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WazeCredit.Models;
+
+namespace WazeCredit.Service
+{
+    public class MarketForecaster
+    {
+        public MarketResult GetMarketPrediction()
+        {
+            //Call API to do some complex calculations and current stock market forecast
+            //For course purpose we will hard code the result
+
+            return new MarketResult
+            {
+                MarketCondition = Models.MarketCondition.StableUp
+            };
+        }
+    }
+
+    public class MarketResult
+    {
+        public MarketCondition MarketCondition { get; set; }
+    }
+}
