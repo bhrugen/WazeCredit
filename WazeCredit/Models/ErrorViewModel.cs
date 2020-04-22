e10e3cc3eac9fecb0010df3e1ae5be830f3e169e
Ben Spark <bhrugen1990@gmail.com>
2020-04-22 13:59:50 -0500

    Add project files.

---
+++ WazeCredit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WazeCredit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
