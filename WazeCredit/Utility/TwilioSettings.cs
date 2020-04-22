d276e0d5580c5999337903fb782b9d7c7d5808df
Ben Spark <bhrugen1990@gmail.com>
2020-04-22 18:44:14 -0500

    AppSettings Setup

---
+++ WazeCredit/Utility/TwilioSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WazeCredit.Utility
+{
+    public class TwilioSettings
+    {
+        public string PhoneNumber { get; set; }
+        public string AuthToken { get; set; }
+        public string AccountSid { get; set; }
+    }
+}
