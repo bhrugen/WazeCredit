3f5b3fc1c2884f8c868a83caae4e9bd94d6c58ad
Ben Spark <bhrugen1990@gmail.com>
2020-04-23 15:34:59 -0500

    Credit validator interface to use multiple services

---
+++ WazeCredit/Service/CreditValidator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WazeCredit.Models;
+
+namespace WazeCredit.Service
+{
+    public class CreditValidator : ICreditValidator
+    {
+        private readonly IEnumerable<IValidationChecker> _validations;
+        public CreditValidator(IEnumerable<IValidationChecker> validations)
+        {
+            _validations = validations;
+        }
+
+        public async Task<(bool, IEnumerable<string>)> PassAllValidations(CreditApplication model)
+        {
+            bool validationsPassed = true;
+
+            List<string> errorMessages = new List<string>();
+
+            foreach(var validation in _validations)
+            {
+                if (!validation.ValidatorLogic(model))
+                {
+                    //Error
+                    errorMessages.Add(validation.ErrorMessage);
+                    validationsPassed = false;
+                }
+            }
+
+            return (validationsPassed, errorMessages);
+        }
+    }
+}
---
+++ WazeCredit/Service/ICreditValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WazeCredit.Models;
+
+namespace WazeCredit.Service
+{
+    public interface ICreditValidator
+    {
+        Task<(bool, IEnumerable<string>)> PassAllValidations(CreditApplication model);
+    }
+}
