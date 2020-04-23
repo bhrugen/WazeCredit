95a8b244e8e7d058971520d623083e478450c23b
Ben Spark <bhrugen1990@gmail.com>
2020-04-23 14:00:23 -0500

    3 Project Setup Part 2

--- WazeCredit/Controllers/HomeController.cs
+++ WazeCredit/Controllers/HomeController.cs
@@ -21,6 +21,8 @@ namespace WazeCredit.Controllers
         //private readonly SendGridSettings _sendGridOptions;
         //private readonly TwilioSettings _twilioOptions;
         private readonly WazeForecastSettings _wazeOptions;
+        [BindProperty]
+        private CreditApplication CreditModel { get; set; }
 
         public HomeController(IMarketForecaster marketForecaster, IOptions<WazeForecastSettings> wazeOptions)
         {
@@ -70,6 +72,15 @@ namespace WazeCredit.Controllers
             messages.Add($"Twilio Token: " + twilioOptions.Value.AuthToken);
             return View(messages);
         }
+
+
+        public IActionResult CreditApplication()
+        {
+            CreditModel = new CreditApplication();
+            return View(CreditModel);
+        }
+
+
 
 
         public IActionResult Privacy()
---
+++ WazeCredit/Models/ViewModels/CreditResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WazeCredit.Models.ViewModels
+{
+    public class CreditResult
+    {
+        public bool Success { get; set; }
+        public IEnumerable<String> ErrorList { get; set; }
+        public int CreditID { get; set; }
+        public double CreditApproved { get; set; }
+    }
+}
