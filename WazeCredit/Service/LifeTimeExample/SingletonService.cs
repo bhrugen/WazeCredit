7ebe89c584554b6f434c7102c52582649248abac
Ben Spark <bhrugen1990@gmail.com>
2020-04-22 19:37:50 -0500

    Create Lifetime Classes

---
+++ WazeCredit/Service/LifeTimeExample/SingletonService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WazeCredit.Service.LifeTimeExample
+{
+    public class SingletonService
+    {
+        private readonly Guid guid;
+
+        public SingletonService()
+        {
+            guid = Guid.NewGuid();
+        }
+        public string GetGuid() => guid.ToString();
+    }
+}
---
+++ WazeCredit/Service/LifeTimeExample/TransientService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WazeCredit.Service.LifeTimeExample
+{
+    public class TransientService
+    {
+        private readonly Guid guid;
+
+        public TransientService()
+        {
+            guid = Guid.NewGuid();
+        }
+        public string GetGuid() => guid.ToString();
+    }
+}
